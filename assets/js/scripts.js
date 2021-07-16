c5a08d813dd43b3c06af67a7c43b00890271acd4
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-07-16 12:55:23 +0200

    Done with first to grid items + nav + header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,23 +9,3 @@ function burgerToggle() {
         navLinks.style.display = "block";
     }
 }
-
-// Scroll to top button, based on example from w3schools (https://www.w3schools.com/howto/howto_js_scroll_to_top.asp)
-toTopBtn = document.querySelector("#toTopBtn");
-
-// Denne kode gør at knappen dukker op når brugeren har rullet 20px ned ad siden
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    toTopBtn.style.display = "block";
-  } else {
-    toTopBtn.style.display = "none";
-  }
-}
-
-// Ved tryg på knappen vil brugeren blive scrollet tilbage til toppen af dokumentet
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
